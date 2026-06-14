4bc1afeb3d2565b8ee861d5ee38b0ddfa81491ca
Abdoulie Bah <bahablie03@gmail.com>
2026-06-14 14:04:13 +0000

    fix: use verified sender email for Brevo

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,13 +1,6 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async ({ to, subject, html }) => {
-  console.log('=== EMAIL DEBUG ===');
-  console.log('HOST:', process.env.EMAIL_HOST);
-  console.log('PORT:', process.env.EMAIL_PORT);
-  console.log('USER:', process.env.EMAIL_USER);
-  console.log('PASS exists:', !!process.env.EMAIL_PASS);
-  console.log('TO:', to);
-
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: parseInt(process.env.EMAIL_PORT),
@@ -22,23 +15,19 @@ const sendEmail = async ({ to, subject, html }) => {
     }
   });
 
+  const mailOptions = {
+    from: `"UTG Attendance System" <${process.env.EMAIL_FROM}>`,
+    to,
+    subject,
+    html
+  };
+
   try {
-    console.log('Verifying transporter...');
-    await transporter.verify();
-    console.log('Transporter verified! Sending email...');
-
-    const info = await transporter.sendMail({
-      from: `"UTG Attendance System" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      html
-    });
-
+    const info = await transporter.sendMail(mailOptions);
     console.log('Email sent:', info.messageId);
     return info;
   } catch (error) {
     console.error('Email error:', error.message);
-    console.error('Full error:', JSON.stringify(error, null, 2));
     throw error;
   }
 };
